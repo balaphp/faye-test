5dbcb51e5128cfa4ceb7365c90fa322a595cfce6
balaganesh k. <balaganesh.k89@gmail.com>
2014-03-03 15:36:38 +0530

    Create benchmark.js

---
+++ socket.io/benchmark.js
@@ -0,0 +1,69 @@
+(function() {
+
+   /**
+    * Before connection (optional, just for faye)
+    * @param {client} client connection
+    */
+   exports.beforeConnect = function(client) {
+     // Example:
+     // client.setHeader('Authorization', 'OAuth abcd-1234');
+     // client.disable('websocket');
+   };
+
+   /**
+    * On client connection (required)
+    * @param {client} client connection
+    * @param {done} callback function(err) {}
+    */
+   exports.onConnect = function(client, done) {
+     // Faye client
+    
+
+     // Socket.io client
+      setInterval(function(){ 
+    	  client.emit('test', { message: getDateTime() });
+	   },1000);
+
+     // Primus client
+     // client.write('Sailing the seas of cheese');
+      function getDateTime() {
+
+		    var date = new Date();
+
+		    var hour = date.getHours();
+		    hour = (hour < 10 ? "0" : "") + hour;
+
+		    var min  = date.getMinutes();
+		    min = (min < 10 ? "0" : "") + min;
+
+		    var sec  = date.getSeconds();
+		    sec = (sec < 10 ? "0" : "") + sec;
+
+		    var year = date.getFullYear();
+
+		    var month = date.getMonth() + 1;
+		    month = (month < 10 ? "0" : "") + month;
+
+		    var day  = date.getDate();
+		    day = (day < 10 ? "0" : "") + day;
+
+		    return year + ":" + month + ":" + day + ":" + hour + ":" + min + ":" + sec;
+
+		}
+
+     //done();
+   };
+
+   /**
+    * Send a message (required)
+    * @param {client} client connection
+    * @param {done} callback function(err) {}
+    */
+   exports.sendMessage = function(client, done) {
+     // Example:
+     // client.emit('test', { hello: 'world' });
+     // client.publish('/test', { hello: 'world' });
+     done();
+   };
+
+})();
